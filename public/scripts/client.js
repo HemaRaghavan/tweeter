46f1bc503ccdcc0e378ef7ff8e86bfd21709ccb1
HemaRaghavan <pvhemaraghavan@gmail.com>
2020-10-09 01:24:25 +0000

    Form submission using jQuery and fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     output += `</header>`;
     output += `<p class="tweet-content">`;
     output += `${postObj.content.text}</p>`;
-    output +=  `<footer class="footer"> 10 days ago <a class="icon">
+    output +=  `<footer class="footer"> ${postObj.created_at} <a class="icon">
                 <i class="fa fa-flag" aria-hidden="true"></i>
                 <i class="fa fa-retweet" aria-hidden="true"></i>
                 <i class="fa fa-heart" aria-hidden="true"></i>
@@ -32,32 +32,32 @@ $(document).ready(function() {
     }
   }
   
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET' 
+    })
+    .then(function (data) {
+        renderTweets(data);
+    })
+    
+  }
+  loadTweets();
+
+  $("#form_submit").submit(function(event) {
+     // prevent the default behaviour to leave the page
+    event.preventDefault();                               
+
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: $(this).serialize()
+    }).then(function(data) {
+        console.log("success");
+    })
+    
+    
+  });
 
 });
 
